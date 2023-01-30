c7b45d788aaf60a4430ba3b4efb5512d0e905d2b
unknown <belekobdev>
2023-01-30 21:52:19 +1300

    init

---
+++ dom.js
@@ -0,0 +1,381 @@
+
+const headerId = document.querySelector("#header")
+headerId.style.position = "sticky"
+headerId.style.top = "0"
+headerId.style.width = "100%"
+headerId.style.background = "#ffffff%"
+headerId.style.zIndex = "99"
+headerId.style.padding = "20px 0"
+headerId.style.boxShadow = "0 4px 4px rgb(0, 0, 0, 0.4)"
+headerId.style.backdropFilter = "blur(4px)"
+
+const headerDiv = document.querySelector(".header")
+headerDiv.style.display = "flex"
+headerDiv.style.justifyContent = "space-between"
+headerDiv.style.alignItems = "center"
+headerDiv.style.animation = "head 2s ease-in-out"
+
+const headerNav = document.querySelector(".header-nav")
+
+headerNav.style.display = "flex"
+headerNav.style.alignItems = "center"
+headerNav.style.justifyContent = "space-between"
+headerNav.style.width = "600px"
+
+const headerA = document.querySelectorAll("a")
+
+headerA[0].style.fontWeight = "400"
+headerA[0].style.fontSize = "14px"
+headerA[0].style.color = "rgba(31, 46, 53, 0.8)"
+headerA[0].style.transition = "0.4s"
+const headerA1 = document.querySelectorAll("a")
+
+headerA1[1].style.fontWeight = "400"
+headerA1[1].style.fontSize = "14px"
+headerA1[1].style.color = "rgba(31, 46, 53, 0.8)"
+headerA1[1].style.transition = "0.4s"
+const headerA2 = document.querySelectorAll("a")
+
+headerA2[2].style.fontWeight = "400"
+headerA2[2].style.fontSize = "14px"
+headerA2[2].style.color = "rgba(31, 46, 53, 0.8)"
+headerA2[2].style.transition = "0.4s"
+const headerA3 = document.querySelectorAll("a")
+
+headerA3[3].style.fontWeight = "400"
+headerA3[3].style.fontSize = "14px"
+headerA3[3].style.color = "rgba(31, 46, 53, 0.8)"
+headerA3[3].style.transition = "0.4s"
+
+const headerSelect = document.querySelectorAll("select")
+
+headerSelect[0].style.fontWeight = "400"
+headerSelect[0].style.fontSize = "14px"
+headerSelect[0].style.color = "rgba(31, 46, 53, 0.8)"
+headerSelect[0].style.transition = "0.4s"
+headerSelect[0].style.outline = "none"
+
+const headerSelect1 = document.querySelectorAll("select")
+
+headerSelect1[1].style.fontWeight = "400"
+headerSelect1[1].style.fontSize = "14px"
+headerSelect1[1].style.color = "rgba(31, 46, 53, 0.8)"
+headerSelect1[1].style.transition = "0.4s"
+headerSelect1[1].style.outline = "none"
+
+const headerA4 = document.querySelectorAll("a")
+
+headerA4[4].style.fontSize = "22px"
+headerA4[4].style.margin = "0 10px"
+headerA4[4].style.color = "rgba(31, 46, 53, 0.8)"
+
+const headerA5 = document.querySelectorAll("a")
+
+headerA5[5].style.fontSize = "22px"
+headerA5[5].style.margin = "0 10px"
+headerA5[5].style.color = "rgba(31, 46, 53, 0.8)"
+
+const headerA6 = document.querySelectorAll("a")
+
+headerA6[6].style.fontSize = "22px"
+headerA6[6].style.margin = "0 10px"
+headerA6[6].style.color = "rgba(31, 46, 53, 0.8)"
+
+
+const heroId = document.querySelector("#hero")
+
+heroId.style.padding = "100px 0"
+
+const heroDiv = document.querySelector(".hero")
+
+heroDiv.style.display = "flex"
+heroDiv.style.alignItems = "center"
+heroDiv.style.justifyContent = "space-between"
+heroDiv.style.paddingTop = "40px"
+
+const heroh1 = document.querySelector("h1")
+
+heroh1.style.fontWeight = "700"
+heroh1.style.fontSize = "48px"
+heroh1.style.lineHeight = "133.69%"
+heroh1.style.color = "#1F2E35"
+
+const heroh1Span = document.querySelectorAll("span")
+
+heroh1Span[0].style.color = "#EE4248"
+heroh1Span[1].style.color = "#EE4248"
+heroh1Span[2].style.color = "#EE4248"
+heroh1Span[3].style.color = "#EE4248"
+
+const heroh1p = document.querySelectorAll("p")
+
+heroh1p[0].style.fontWeight = "400"
+heroh1p[0].style.fontSize = "20px"
+heroh1p[0].style.lineHeight = "29px"
+heroh1p[0].style.color = "#30444E"
+heroh1p[0].style.padding = "20px 0"
+
+heroh1p[1].style.fontWeight = "400"
+heroh1p[1].style.fontSize = "18px"
+heroh1p[1].style.lineHeight = "150%"
+heroh1p[1].style.color = "#30444E"
+
+heroh1p[2].style.fontWeight = "400"
+heroh1p[2].style.fontSize = "18px"
+heroh1p[2].style.lineHeight = "150%"
+heroh1p[2].style.color = "#30444E"
+heroh1p[2].style.paddingBottom = "15px"
+
+heroh1p[3].style.fontWeight = "400"
+heroh1p[3].style.fontSize = "18px"
+heroh1p[3].style.lineHeight = "150%"
+heroh1p[3].style.color = "#30444E"
+heroh1p[3].style.paddingBottom = "15px"
+
+heroh1p[4].style.fontWeight = "400"
+heroh1p[4].style.fontSize = "18px"
+heroh1p[4].style.lineHeight = "150%"
+heroh1p[4].style.color = "#30444E"
+heroh1p[4].style.paddingBottom = "15px"
+
+heroh1p[5].style.fontWeight = "300"
+heroh1p[5].style.fontSize = "18px"
+heroh1p[5].style.lineHeight = "149%"
+heroh1p[5].style.color = "#263238"
+
+heroh1p[6].style.fontWeight = "300"
+heroh1p[6].style.fontSize = "18px"
+heroh1p[6].style.lineHeight = "149%"
+heroh1p[6].style.color = "#263238"
+
+const heroBtn = document.querySelectorAll("button")
+
+heroBtn[0].style.width = "161px"
+heroBtn[0].style.height = "53px"
+heroBtn[0].style.background = "#EE4248"
+heroBtn[0].style.boxShadow = "1px 2px 4px rgba(0, 0, 0, 0.25)"
+heroBtn[0].style.borderRadius = "4px"
+heroBtn[0].style.fontWeight = "400"
+heroBtn[0].style.fontSize = "20px"
+heroBtn[0].style.color = "#FFFFFF"
+heroBtn[0].style.cursor = "pointer"
+
+heroBtn[1].style.width = "119px"
+heroBtn[1].style.height = "42px"
+heroBtn[1].style.background = "#EE4248"
+heroBtn[1].style.boxShadow = "1px 2px 2px rgba(0, 0, 0, 0.25)"
+heroBtn[1].style.borderRadius = "4px"
+heroBtn[1].style.fontWeight = "400"
+heroBtn[1].style.fontSize = "18px"
+heroBtn[1].style.color = "#FFFFFF"
+heroBtn[1].style.cursor = "pointer"
+
+heroBtn[2].style.width = "119px"
+heroBtn[2].style.height = "42px"
+heroBtn[2].style.background = "#EE4248"
+heroBtn[2].style.boxShadow = "1px 2px 2px rgba(0, 0, 0, 0.25)"
+heroBtn[2].style.borderRadius = "4px"
+heroBtn[2].style.fontWeight = "400"
+heroBtn[2].style.fontSize = "18px"
+heroBtn[2].style.color = "#FFFFFF"
+heroBtn[2].style.cursor = "pointer"
+
+heroBtn[3].style.width = "119px"
+heroBtn[3].style.height = "42px"
+heroBtn[3].style.background = "#EE4248"
+heroBtn[3].style.boxShadow = "1px 2px 2px rgba(0, 0, 0, 0.25)"
+heroBtn[3].style.borderRadius = "4px"
+heroBtn[3].style.fontWeight = "400"
+heroBtn[3].style.fontSize = "18px"
+heroBtn[3].style.color = "#FFFFFF"
+heroBtn[3].style.cursor = "pointer"
+
+
+const compId = document.querySelector("#comp")
+
+compId.style.background = "#F7F7F7"
+compId.style.padding = "100px 0"
+
+const compDiv = document.querySelector(".comp")
+
+compDiv.style.display = "flex"
+compDiv.style.justifyContent = "space-between"
+compDiv.style.alignItems = "center"
+
+const comph1 = document.querySelectorAll("h1")
+const comph2 = document.querySelectorAll("h3")
+comph2[0].style.fontWeight = "500"
+comph2[0].style.fontSize = "18px"
+comph2[0].style.color = "#263238"
+comph2[0].style.padding = "10px 0"
+
+
+
+
+comph1[1].style.fontWeight = "500"
+comph1[1].style.color = "#1F2E35"
+comph1[1].style.fontSize = "32px"
+comph1[1].style.lineHeight = "150%"
+comph1[1].style.padding = "20px 0"
+
+comph1[3].style.fontWeight = "500"
+comph1[3].style.color = "#1F2E35"
+comph1[3].style.fontSize = "28px"
+comph1[3].style.lineHeight = "133%"
+comph1[3].style.padding = "20px 0"
+
+comph1[4].style.fontWeight = "500"
+comph1[4].style.color = "#1F2E35"
+comph1[4].style.fontSize = "28px"
+comph1[4].style.lineHeight = "133%"
+comph1[4].style.padding = "20px 0"
+
+comph1[5].style.fontWeight = "500"
+comph1[5].style.color = "#1F2E35"
+comph1[5].style.fontSize = "28px"
+comph1[5].style.lineHeight = "133%"
+comph1[5].style.padding = "20px 0"
+
+comph1[6].style.fontWeight = "500"
+comph1[6].style.color = "#263238"
+comph1[6].style.fontSize = "24px"
+comph1[6].style.padding = "30px 0"
+
+comph1[7].style.fontWeight = "400"
+comph1[7].style.color = "#263238"
+comph1[7].style.fontSize = "18px"
+comph1[7].style.paddingTop = "30px "
+
+
+const blocksId = document.querySelector("#blocks")
+
+blocksId.style.padding = "100px 0"
+
+const blocksDiv = document.querySelector(".blocks")
+
+blocksDiv.style.display = "flex"
+blocksDiv.style.alignItems = "center"
+blocksDiv.style.justifyContent = "space-between"
+
+const blocksTit = document.querySelector(".blocks-title")
+
+blocksTit.style.fontWeight = "500"
+blocksTit.style.fontSize = "32px"
+blocksTit.style.textAlign = "center"
+blocksTit.style.color = "#1F2E35"
+blocksTit.style.paddingBottom = "90px"
+
+
+const contactId = document.querySelector("#contact")
+
+contactId.style.padding = "100px 0"
+
+const contactDiv = document.querySelector(".contact")
+
+contactDiv.style.display = "flex"
+contactDiv.style.alignItems = "center"
+contactDiv.style.justifyContent = "space-between"
+contactDiv.style.width = "700px"
+
+const footerId = document.querySelector("#footer")
+
+footerId.style.background = "#EE4248"
+footerId.style.height = "70px"
+
+const footerDiv = document.querySelector(".footer")
+
+footerDiv.style.display = "flex"
+footerDiv.style.alignItems = "center"
+footerDiv.style.justifyContent = "center"
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const title = document.querySelector("#title")
+// title.style.fontSize = "50px"
+// title.style.fontWeight = "600px"
+// title.style.color = "yellow"
+// title.style.textTransform = "uppercase"
+// // 2 - console.log(document.getElementById("title"))
+//
+// const tag = document.querySelector("p")
+// tag.style.fontSize = "20px"
+// tag.style.fontWeight = "20px"
+// tag.style.color = "red"
+//
+// // 2 - console.log(document.getElementByTagName("p"))
+//
+// const btn = document.querySelector(".btn")
+// btn.style.background = "red"
+// btn.style.border = "3px solid yellow"
+// btn.style.fontSize = "18px"
+// btn.style.color = "yellow"
+// btn.style.width = "140px"
+// btn.style.height = "50px"
+// btn.style.borderRadius = "14px"
+// btn.style.cursor = "pointer"
+//
+//
+// // 2 - console.log(document.getElementsByClassName("btn"))
+//
+// const All = document.querySelector(".all")
+// All.style.display = "flex"
+// All.style.justifyContent = "center"
+// All.style.textAlign = "center"
+// All.style.flexDirection = "column"
+//
+// const input = document.querySelectorAll("input")
+// input[0].style.margin = "0px 50px"
+// input[0].style.padding = "0px 10px"
+// input[0].style.width = "200px"
+// input[0].style.height = "40px"
+// input[0].style.color = "red"
+// input[0].style.fontWeight = "400px"
+// input[0].style.fontSize = "16px"
+// input[0].style.border = "4px solid red"
+// input[0].style.borderRadius = "8px"
+// input[0].style.background = "yellow"
+// input[0].style.outline = "none"
+//
+// const inputs = document.querySelectorAll("input")
+// inputs[1].style.margin = "0px 50px"
+// inputs[1].style.padding = "0px 10px"
+// inputs[1].style.width = "200px"
+// inputs[1].style.height = "40px"
+// inputs[1].style.color = "red"
+// inputs[1].style.fontWeight = "400px"
+// inputs[1].style.fontSize = "16px"
+// inputs[1].style.border = "4px solid red"
+// inputs[1].style.borderRadius = "8px"
+// inputs[1].style.background = "yellow"
+// inputs[1].style.outline = "none"
+//
+// const glav = document.querySelector(".glav")
+// glav.style.display = "flex"
+// glav.style.justifyContent = "center"
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
